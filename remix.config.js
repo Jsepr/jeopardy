945e916f9363021016ba5cf66619f0edd045ba3e
Jesper Magnusson <jespermagnusson2@gmail.com>
2023-09-30 17:43:14 +0200

    Update remix dependencies

--- remix.config.js
+++ remix.config.js
@@ -5,11 +5,11 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  tailwind: true,
   future: {
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
-    unstable_tailwind: true,
   },
 };
